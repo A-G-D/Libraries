97a7f9a83972f307034970df3215f098f1cb3ad0
A-G-D <aloeverdulay@live.com>
2017-07-24 15:51:32 +0800

    Circular Doubly-Linked Template List

--- CPP/linkedlist.hpp
+++ CPP/linkedlist.hpp
@@ -1,6 +1,6 @@
 /*******************************************************************************
 *
-*	linkedlist.h
+*	linkedlist.hpp
 *
 *
 *	List<T>
@@ -19,11 +19,11 @@
 *			List(const List<type> &list)
 *				- Copy constructor
 *
-*			Node<type> first()
+*			List<type>::Node first()
 *				- Front element of the list
-*			Node<type> last()
+*			List<type>::Node last()
 *				- Back element of the list
-*			Node<type> head()
+*			List<type>::Node head()
 *				- Head node - usually used to mark the end of iteration
 *				  i.e., when the current node equals this (head) node
 *
@@ -33,23 +33,23 @@
 *			bool empty()
 *				- Checks if the list is empty or not
 *
-*			Node<type> push(type value)
+*			List<type>::Node push(type value)
 *				- Adds a new element to the front of the list and returns a new
 *				  node correspoding to the added element
-*			Node<type> push(List<type> &list)
+*			List<type>::Node push(List<type> &list)
 *				- Transfers all the elements from the input list to the front of
 *				  this list and returns the first element of the input list
-*			void pop()
-*				- Removes the front element of the list
+*			type pop()
+*				- Removes the front element of the list and returns its value
 *
-*			Node<type> enqueue(type value)
+*			List<type>::Node enqueue(type value)
 *				- Adds a new element to the back of the list and returns a new
 *				  node corresponding to the added element
-*			Node<type> enqueue(List<type> &list)
+*			List<type>::Node enqueue(List<type> &list)
 *				- Transfers all the elements from the input list to the back of
 *				  this list and returns the first element of the input list
-*			void eject()
-*				- Removes the back element of the list
+*			type eject()
+*				- Removes the back element of the list and returns its value
 *
 *			void rotate_left()
 *			void rotate_right()
@@ -122,33 +122,33 @@
 *				- Compares the sizes of two lists
 *
 *
-*		class Node<type>
+*		class List<type>::Node
 *
-*			Node<type>(Node<type> *node)
+*			List<type>::Node(List<type>::Node *node)
 *				- Constructs a node based on node pointer argument
 *
-*			static Node<type>& null()
+*			static List<type>::Node& null()
 *				- NULL node (constant)
 *
 *			type& data()
 *				- Node data
 *
-*			Node<type> prev()
-*			Node<type> next()
+*			List<type>::Node prev()
+*			List<type>::Node next()
 *				- The previous/next node of this node
 *
-*			Node<type> insert(type value)
+*			List<type>::Node insert(type value)
 *				- Appends a new element next to this node
-*			Node<type> insert(List<type> &list)
+*			List<type>::Node insert(List<type> &list)
 *				- Appends all the elements of the input list next to this node
 *				- Elements of the input list are not copied but transfered
-*			void remove()
-*				- Removes this node from its list
+*			type remove()
+*				- Removes this node from its list and returns its value
 *
-*			void swap(Node<type> &node)
+*			void swap(List<type>::Node &node)
 *				- Swaps the position of two nodes
 *
-*			void move(Node<type> &node)
+*			void move(List<type>::Node &node)
 *				- Moves this node next to the input node
 *
 *			void move_left()
@@ -156,190 +156,192 @@
 *				- Moves this node clockwise/counter-clockwise by 1 bit
 *				- left -> counter-clockwise | right -> clockwise
 *
-*			Node<type>& operator<<(int bits)
-*			Node<type>& operator>>(int bits)
+*			List<type>::Node& operator<<(int bits)
+*			List<type>::Node& operator>>(int bits)
 *				- Moves this node clockwise/counter-clockwise by a certain number
 *				  of bits
 *
-*			Node<type> operator++()
-*			Node<type> operator++(int)
+*			List<type>::Node operator++()
+*			List<type>::Node operator++(int)
 *				- Moves on to the next node
 *
-*			Node<type> operator--()
-*			Node<type> operator--(int)
+*			List<type>::Node operator--()
+*			List<type>::Node operator--(int)
 *				- Moves on to the previous node
 *
-*			bool operator==(Node<type> &node)
-*			bool operator!=(Node<type> &node)
+*			bool operator==(List<type>::Node &node)
+*			bool operator!=(List<type>::Node &node)
 *				- Checks the equality of two nodes
 *
 *******************************************************************************/
-#ifndef __LINKEDLIST_H__
-#define __LINKEDLIST_H__
+#ifndef __LINKEDLIST_HPP__
+#define __LINKEDLIST_HPP__
 
 
 #include <stdlib.h>
 #include <time.h>
 
 
-template <class T> class List;
-
-
-template <class T> class Node {
-
-
-	friend class List<T>;
-
-	Node<T> *p_ptr,
-		 	*p_prev,
-		 	*p_next;
-	T p_data;
-
-	inline Node(Node<T> *node) {
-		this->p_ptr = node;
-	}
-
-	void link(Node<T> *node) const {
-		Node<T> *ptr = this->p_ptr,
-			 	*next = ptr->p_next;
-		next->p_prev = node;
-		ptr->p_next = node;
-		node->p_prev = ptr;
-		node->p_next = next;
-	}
-	void unlink() const {
-		Node<T> *ptr = this->p_ptr,
-			 	*prev = ptr->p_prev,
-			 	*next = ptr->p_next;
-		next->p_prev = prev;
-		prev->p_next = next;
-	}
-
-public:
-
-	static Node<T>& null() {
-		static Node<T> null_node(NULL);
-		return null_node;
-	}
-
-	inline T& data() const {
-		return this->p_ptr->p_data;
-	}
-	inline Node<T> prev() const {
-		return Node<T>(this->p_ptr->p_prev);
-	}
-	inline Node<T> next() const {
-		return Node<T>(this->p_ptr->p_next);
-	}
-
-	Node<T> insert(const T &data) const {
-		Node<T> *node = new Node<T>;
-		this->link(node);
-		node->p_data = data;
-		return Node<T>(node);
-	}
-	Node<T> insert(const List<T> &list) const {
-		Node<T> *ptr = this->p_ptr,
-			 	*next = ptr->p_next,
-			 	*head = list.p_head.p_ptr,
-				*first = head->p_next,
-				*last = head->p_prev;
-		first->p_prev = ptr;
-		ptr->p_next = first;
-		next->p_prev = last;
-		last->p_next = next;
-		head->p_prev = head;
-		head->p_next = head;
-		return Node<T>(first);
-	}
-	void remove() const {
-		this->unlink();
-		delete this->p_ptr;
-	}
-
-	void swap(const Node<T> &node) const {
-		Node<T> *ptr = this->p_ptr,
-			 	*other = node.p_ptr,
-			 	*this_prev = ptr->p_prev,
-			 	*this_next = ptr->p_next,
-			 	*node_prev = other->p_prev,
-			 	*node_next = other->p_next;
-		ptr->p_prev = node_prev;
-		ptr->p_next = node_next;
-		other->p_prev = this_prev;
-		other->p_next = this_next;
-		this_next->p_prev = other;
-		this_prev->p_next = other;
-		node_next->p_prev = ptr;
-		node_prev->p_next = ptr;
-	}
-
-	void move(const Node<T> &node) const {
-		this->unlink();
-		node.link(this->p_ptr);
-	}
-	inline void move_left() const {
-		this->move(this->prev().prev());
-	}
-	inline void move_right() const {
-		this->move(this->next());
-	}
-
-	Node<T>& operator<<(int bits) {
-		while(bits-- > 0)
-			this->move_left();
-		return *this;
-	}
-	Node<T>& operator>>(int bits) {
-		while(bits-- > 0)
-			this->move_right();
-		return *this;
-	}
-
-	Node<T>& operator++() {
-		this->p_ptr = this->p_ptr->p_next;
-		return *this;
-	}
-	Node<T> operator++(int) {
-		Node<T> temp = *this;
-		this->p_ptr = this->p_ptr->p_next;
-		return temp;
-	}
-
-	Node<T>& operator--() {
-		this->p_ptr = this->p_ptr->p_prev;
-		return *this;
-	}
-	Node<T> operator--(int) {
-		Node<T> temp = *this;
-		this->p_ptr = this->p_ptr->p_prev;
-		return temp;
-	}
-
-	Node<T>& operator=(const Node<T> &node) {
-		this->p_ptr = node.p_ptr;
-		return *this;
-	}
-
-	inline bool operator==(const Node<T> &node) const {
-		return this->p_ptr == node.p_ptr;
-	}
-	inline bool operator!=(const Node<T> &node) const {
-		return this->p_ptr != node.p_ptr;
-	}
-
-	inline Node() {}
-
-
-};
-
-
 template <class T> class List {
 
 
-	friend class Node<T>;
+public:
 
-	Node<T> p_head;
+	class Node {
+
+
+		friend class List<T>;
+
+		Node *p_ptr,
+			 *p_prev,
+			 *p_next;
+		T p_data;
+
+		inline Node(Node *node) {
+			this->p_ptr = node;
+		}
+
+		void link(Node *node) const {
+			Node *ptr = this->p_ptr,
+				 *next = ptr->p_next;
+			next->p_prev = node;
+			ptr->p_next = node;
+			node->p_prev = ptr;
+			node->p_next = next;
+		}
+		void unlink() const {
+			Node *ptr = this->p_ptr,
+				 *prev = ptr->p_prev,
+				 *next = ptr->p_next;
+			next->p_prev = prev;
+			prev->p_next = next;
+		}
+
+	public:
+
+		static Node& null() {
+			static Node null_node(NULL);
+			return null_node;
+		}
+
+		inline T& data() const {
+			return this->p_ptr->p_data;
+		}
+		inline Node prev() const {
+			return Node(this->p_ptr->p_prev);
+		}
+		inline Node next() const {
+			return Node(this->p_ptr->p_next);
+		}
+
+		Node insert(const T &data) const {
+			Node *node = new Node;
+			this->link(node);
+			node->p_data = data;
+			return Node(node);
+		}
+		Node insert(const List<T> &list) const {
+			Node *ptr = this->p_ptr,
+				 *next = ptr->p_next,
+				 *head = list.p_head.p_ptr,
+				 *first = head->p_next,
+				 *last = head->p_prev;
+			first->p_prev = ptr;
+			ptr->p_next = first;
+			next->p_prev = last;
+			last->p_next = next;
+			head->p_prev = head;
+			head->p_next = head;
+			return Node(first);
+		}
+		T remove() const {
+			T data = this->p_ptr->p_data;
+			this->unlink();
+			delete this->p_ptr;
+			return data;
+		}
+
+		void swap(const Node &node) const {
+			Node *ptr = this->p_ptr,
+				 *other = node.p_ptr,
+				 *this_prev = ptr->p_prev,
+				 *this_next = ptr->p_next,
+				 *node_prev = other->p_prev,
+				 *node_next = other->p_next;
+			ptr->p_prev = node_prev;
+			ptr->p_next = node_next;
+			other->p_prev = this_prev;
+			other->p_next = this_next;
+			this_next->p_prev = other;
+			this_prev->p_next = other;
+			node_next->p_prev = ptr;
+			node_prev->p_next = ptr;
+		}
+
+		void move(const Node &node) const {
+			this->unlink();
+			node.link(this->p_ptr);
+		}
+		inline void move_left() const {
+			this->move(this->prev().prev());
+		}
+		inline void move_right() const {
+			this->move(this->next());
+		}
+
+		Node& operator<<(int bits) {
+			while(bits-- > 0)
+				this->move_left();
+			return *this;
+		}
+		Node& operator>>(int bits) {
+			while(bits-- > 0)
+				this->move_right();
+			return *this;
+		}
+
+		Node& operator++() {
+			this->p_ptr = this->p_ptr->p_next;
+			return *this;
+		}
+		Node operator++(int) {
+			Node temp = *this;
+			this->p_ptr = this->p_ptr->p_next;
+			return temp;
+		}
+
+		Node& operator--() {
+			this->p_ptr = this->p_ptr->p_prev;
+			return *this;
+		}
+		Node operator--(int) {
+			Node temp = *this;
+			this->p_ptr = this->p_ptr->p_prev;
+			return temp;
+		}
+
+		Node& operator=(const Node &node) {
+			this->p_ptr = node.p_ptr;
+			return *this;
+		}
+
+		inline bool operator==(const Node &node) const {
+			return this->p_ptr == node.p_ptr;
+		}
+		inline bool operator!=(const Node &node) const {
+			return this->p_ptr != node.p_ptr;
+		}
+
+		inline Node() {}
+
+
+	};
+
+private:
+
+	friend class Node;
+
+	Node p_head;
 
 	static int random_int(int lowbound, int upperbound) {
 		int n = rand();
@@ -349,21 +351,21 @@ template <class T> class List {
 	}
 
 	void add(const List<T> &list) {
-		Node<T> head = list.head(),
-			    last = this->last();
-		for(Node<T> node = head.prev(); node != head; --node)
+		Node head = list.head(),
+			 last = this->last();
+		for(Node node = head.prev(); node != head; --node)
 			last.insert(node.data());
 	}
 
 public:
 
-	inline Node<T> head() const {
-		return Node<T>(this->p_head.p_ptr);
+	inline Node head() const {
+		return Node(this->p_head.p_ptr);
 	}
-	inline Node<T> first() const {
+	inline Node first() const {
 		return this->head().next();
 	}
-	inline Node<T> last() const {
+	inline Node last() const {
 		return this->head().prev();
 	}
 
@@ -373,37 +375,37 @@ public:
 
 	int size() const {
 		int count = 0;
-		Node<T> *head = this->p_head.p_ptr;
-		for(Node<T> *node = head->p_next; node != head; node = node->p_next)
+		Node *head = this->p_head.p_ptr;
+		for(Node *node = head->p_next; node != head; node = node->p_next)
 			++count;
 		return count;
 	}
 
-	inline Node<T> push(const T &data) const {
+	inline Node push(const T &data) const {
 		return this->head().insert(data);
 	}
-	inline Node<T> push(const List<T> &list) const {
+	inline Node push(const List<T> &list) const {
 		return this->head().insert(list);
 	}
-	inline void pop() const {
-		this->first().remove();
+	inline T pop() const {
+		return this->first().remove();
 	}
 
-	inline Node<T> enqueue(const T &data) const {
+	inline Node enqueue(const T &data) const {
 		return this->last().insert(data);
 	}
-	inline Node<T> enqueue(const List<T> &list) const {
+	inline Node enqueue(const List<T> &list) const {
 		return this->last().insert(list);
 	}
-	inline void eject() const {
-		this->last().remove();
+	inline T eject() const {
+		return this->last().remove();
 	}
 
 	void rotate_left() const {
-		Node<T> *head = this->p_head.p_ptr,
-			 	*first = head->p_next,
-				*last = head->p_prev,
-				*first_new = first->p_next;
+		Node *head = this->p_head.p_ptr,
+			 *first = head->p_next,
+			 *last = head->p_prev,
+			 *first_new = first->p_next;
 		head->p_prev = first;
 		head->p_next = first_new;
 		first->p_prev = last;
@@ -412,10 +414,10 @@ public:
 		last->p_next = first;
 	}
 	void rotate_right() const {
-		Node<T> *head = this->p_head.p_ptr,
-			 	*first = head->p_next,
-			 	*last = head->p_prev,
-			 	*last_new = last->p_prev;
+		Node *head = this->p_head.p_ptr,
+			 *first = head->p_next,
+			 *last = head->p_prev,
+			 *last_new = last->p_prev;
 		head->p_prev = last_new;
 		head->p_next = last;
 		last->p_prev = head;
@@ -433,14 +435,14 @@ public:
 		this->push(T());
 	}
 
-	Node<T> random_sample() const {
-		Node<T> node = this->head();
+	Node random_sample() const {
+		Node node = this->head();
 		for(int i = random_int(1, this->size()); i-- > 0; ++node);
 		return node;
 	}
-	List<T> random_subset(int size) const {
+	List random_subset(int size) const {
 		List<T> temp, copy(*this);
-		Node<T> head = temp.head();
+		Node head = temp.head();
 		while(size-- > 0)
 			copy.random_sample().move(head);
 		return temp;
@@ -448,10 +450,10 @@ public:
 
 	List<T> split_front() const {
 		List<T> list;
-		Node<T> *list_head = list.p_head.p_ptr,
-				*this_head = this->p_head.p_ptr,
-			 	*this_first,
-				*node = this_head;
+		Node *list_head = list.p_head.p_ptr,
+			 *this_head = this->p_head.p_ptr,
+			 *this_first,
+			 *node = this_head;
 		for(int i = this->size()/2; i-- > 0; node = node->p_next);
 		this_first = node->p_next;
 		this_first->p_prev = this_head;
@@ -464,11 +466,11 @@ public:
 	}
 	List<T> split_back() const {
 		List<T> list;
-		Node<T> *list_head = list.p_head.p_ptr,
-				*this_head = this->p_head.p_ptr,
-				*list_last = this_head->p_prev,
-			 	*list_first,
-				*node = this_head;
+		Node *list_head = list.p_head.p_ptr,
+			 *this_head = this->p_head.p_ptr,
+			 *list_last = this_head->p_prev,
+			 *list_first,
+			 *node = this_head;
 		for(int i = int(this->size()/2.0 + 0.5); i-- > 0; node = node->p_next);
 		list_first = node->p_next;
 		list_head->p_prev = list_last;
@@ -481,10 +483,10 @@ public:
 	}
 	List<T> split_shuffled() const {
 		List<T> list;
-		Node<T> head = this->head(),
-			 	first = head.next(),
-			 	node = first.next(),
-			 	next;
+		Node head = this->head(),
+			 first = head.next(),
+			 node = first.next(),
+			 next;
 		while(node != head && node != first) {
 			next = node.next().next();
 			node.move(list.last());
@@ -494,11 +496,11 @@ public:
 	}
 
 	void reverse() const {
-		Node<T> *head = this->p_head.p_ptr,
-			 	*first = head->p_next,
-			 	*node = first->p_next,
-			 	*last = first,
-			 	*next;
+		Node *head = this->p_head.p_ptr,
+			 *first = head->p_next,
+			 *node = first->p_next,
+			 *last = first,
+			 *next;
 		while(node != head) {
 			next = node->p_next;
 			node->p_prev = head;
@@ -526,7 +528,7 @@ public:
 
 private:
 
-	void random_position(const Node<T> &node) const {
+	void random_position(const Node &node) const {
 		if(rand()%2)
 			node.move(this->head());
 		else
@@ -576,8 +578,8 @@ public:
 				b.first().move(this->last());
 	}
 
-	void traverse(bool (*handler)(const Node<T>&), bool clockwise) const {
-		Node<T> head = this->head(), node;
+	void traverse(bool (*handler)(const Node&), bool clockwise) const {
+		Node head = this->head(), node;
 		if(clockwise)
 			for(node = head.next(); handler(node); ++node) {
 				if(node.next() == head)
@@ -589,8 +591,8 @@ public:
 					node = head;
 			}
 	}
-	void traverse(void (*callback)(const Node<T>&), bool clockwise) const {
-		Node<T> head = this->head(), node;
+	void traverse(void (*callback)(const Node&), bool clockwise) const {
+		Node head = this->head(), node;
 		if(clockwise)
 			for(node = head.next(); node != head; callback(node++));
 		else
@@ -598,7 +600,7 @@ public:
 	}
 
 	void clear() const {
-		Node<T> *head = this->p_head.p_ptr;
+		Node *head = this->p_head.p_ptr;
 		while(head->p_next != head)
 			this->pop();
 	}
@@ -615,8 +617,8 @@ public:
 	}
 
 	T operator[](int index) const {
-		Node<T> *head = this->p_head.p_ptr,
-			    *node = head->p_next;
+		Node *head = this->p_head.p_ptr,
+			 *node = head->p_next;
 		for(int count = 0; node != head && count++ < index; node = node->p_next);
 		if(node == head || index < 0)
 			return T();
@@ -673,7 +675,7 @@ public:
 private:
 
 	void init() {
-		Node<T> *ptr = new Node<T>;
+		Node *ptr = new Node;
 		this->p_head.p_ptr = ptr;
 		ptr->p_prev = ptr;
 		ptr->p_next = ptr;
@@ -685,9 +687,9 @@ public:
 		this->init();
 	}
 	List(const List<T> &list) {
-		Node<T> *head = list.p_head.p_ptr;
+		Node *head = list.p_head.p_ptr;
 		this->init();
-		for(Node<T> *node = head->p_next; node != head; node = node->p_next)
+		for(Node *node = head->p_next; node != head; node = node->p_next)
 			this->enqueue(node->p_data);
 	}
 	~List() {
